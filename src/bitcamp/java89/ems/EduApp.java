f5246d0e9ce21ab3d4cb071c8ff5dbae5a52fdc2
Nina-K <Nina.JY.kim@gmail.com>
2016-10-28 14:42:07 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -8,6 +8,7 @@ public class EduApp {
   public static void main(String[] args) {
     //EduApp에서 사용하는 keyScan을 StudentController와 공유한다.
     TeacherController teacherController = new TeacherController(keyScan);
+    ClassroomController classroomController = new ClassroomController(keyScan);
 
     System.out.println("비트캠프 관리시스템에 오신걸 환영합니다.");
 
@@ -19,6 +20,7 @@ public class EduApp {
       switch (command) {
       case "menu": doMenu(); break;
       case "go 2": teacherController.service(); break;
+      case "go 3": classroomController.service(); break;
       case "quit": System.out.println("Good bye!"); break loop;
       default:
         System.out.println("지원하지 않는 명령어입니다.");
@@ -30,6 +32,7 @@ public class EduApp {
     System.out.println("[메뉴]");
     System.out.println("1. 학생관리");
     System.out.println("2. 강사관리");
+    System.out.println("3. 강의실관리");
     System.out.println("메뉴 이동은 'go 메뉴번호'를 입력하세요.");
   }
 }
